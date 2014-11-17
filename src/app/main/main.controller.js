11a1153a03495628bee859573a2a9e590ceb38be
Marek Kalnik <marekk@theodo.fr>
2014-11-17 13:01:25 +0100

    Add confirmation alert

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('homepage')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $window) {
     $scope.formData = {};
     $scope.sendContactForm = function() {
       $http.post('http://bamlab.fr/contact', $scope.formData);
+
+      $window.alert('Votre message a été bien envoyé, merci!');
+      $scope.formData = {};
     };
   });
