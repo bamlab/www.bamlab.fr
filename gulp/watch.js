9793918e877e8cb4df398d92fb777003c86f9fcf
Marek Kalnik <marekk@theodo.fr>
2014-11-17 11:10:46 +0100

    Import vendor and custom css to allow using the same variables

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 
 gulp.task('watch', ['wiredep', 'styles'] ,function () {
   gulp.watch('src/{app,components}/**/*.scss', ['styles']);
+  gulp.watch('src/{app,components}/*.scss', ['styles']);
   gulp.watch('src/{app,components}/**/*.js', ['scripts']);
   gulp.watch('src/{app,components}/**/*.jade', ['partials', 'scripts']);
   gulp.watch('src/assets/images/**/*', ['images']);
