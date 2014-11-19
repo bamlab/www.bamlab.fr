616e62bb51f2bf66c2708856017cd075fc7c25bf
Marek Kalnik <marekk@theodo.fr>
2014-11-19 18:24:17 +0100

    Add preprod to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,28 @@ ERRORSTRING='Error, no env specified.'
 if [ $# -eq 0 ]
     then
         echo $ERRORSTRING;
+elif [ $1 == "preprod" ]
+    then
+        echo "Deploying on preprod"
+        if [[ -z $2 ]]
+            then
+                echo "Gulping"
+                gulp build
+                echo "Running dry-run"
+                rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./dist/ bam:/var/www/bam-preprod
+        elif [ $2 == "go" ]
+            then
+                echo "Gulping"
+                gulp build
+                echo "Running actual deploy"
+                rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./dist/ bam:/var/www/bam-preprod
+                ssh bam 'chown -R www-data:www-data /var/www/bam'
+        else
+            echo $ERRORSTRING;
+        fi
 elif [ $1 == "prod" ]
     then
+        echo "Deploying on prod"
         if [[ -z $2 ]]
             then
                 echo "Gulping"
