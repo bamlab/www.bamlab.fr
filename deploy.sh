951efd737213fda9268732a2cf42541b33762e52
Marek Kalnik <marekk@theodo.fr>
2014-11-19 17:12:43 +0100

    Add forgotten gulp build to deploy

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,14 @@ elif [ $1 == "prod" ]
     then
         if [[ -z $2 ]]
             then
+                echo "Gulping"
+                gulp build
                 echo "Running dry-run"
                 rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./dist/ bam:/var/www/bam
         elif [ $2 == "go" ]
             then
+                echo "Gulping"
+                gulp build
                 echo "Running actual deploy"
                 rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./dist/ bam:/var/www/bam
                 ssh bam 'chown -R www-data:www-data /var/www/bam'
