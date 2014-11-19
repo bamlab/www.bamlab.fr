bde43dae145d39c68d4a70542ad4d602dab37f0d
Marek Kalnik <marekk@theodo.fr>
2014-11-19 16:58:10 +0100

    Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
-set +x
+#!/bin/bash
 
-gulp build
-scp -r dist/* bam:/var/www/bam
-ssh bam 'chown -R www-data:www-data /var/www/bam'
+ERRORSTRING='Error, no env specified.'
+
+if [ $# -eq 0 ]
+    then
+        echo $ERRORSTRING;
+elif [ $1 == "prod" ]
+    then
+        if [[ -z $2 ]]
+            then
+                echo "Running dry-run"
+                rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./dist/ bam:/var/www/bam
+        elif [ $2 == "go" ]
+            then
+                echo "Running actual deploy"
+                rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./dist/ bam:/var/www/bam
+                ssh bam 'chown -R www-data:www-data /var/www/bam'
+        else
+            echo $ERRORSTRING;
+        fi
+fi
