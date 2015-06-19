9252ef09dfd0fe75a9ad05bf880481f24b861243
Marek Kalnik <marekk@theodo.fr>
2015-06-19 13:08:42 +0200

    Push query parameter in URL when the form is submitted

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('homepage')
-  .controller('MainCtrl', function ($scope, $http, $window) {
+  .controller('MainCtrl', function ($scope, $http, $window, $location) {
     $scope.formData = {};
     $scope.sendContactForm = function() {
-      $http.post('http://bamlab.fr/contact', $scope.formData);
-
-      $window.alert('Votre message a été bien envoyé, merci!');
-      $scope.formData = {};
+      $http.post('http://bamlab.fr/contact', $scope.formData)
+        .success(function(data, status, headers, config) {
+            $location.search({form: 'sent'});
+            $window.alert('Votre message a été bien envoyé, merci!');
+            $scope.formData = {};
+        });
     };
   })
 ;
